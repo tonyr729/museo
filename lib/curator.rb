d5f47c048e0ab4596a437ba330ac2888f606df7e
Tony Robertson <tonyr729@gmail.com>
2018-10-31 10:12:54 -0600

    Refactor tests and methods to convert photo and artist to class instances

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,3 +1,7 @@
+require_relative '../lib/artist'
+require_relative '../lib/photograph'
+
+
 class Curator
 
   attr_reader :artists, :photographs
@@ -8,18 +12,22 @@ class Curator
   end
 
   def add_photographs(photo)
-    @photographs << photo
+    @photographs << Photograph.new(photo)
   end
 
   def add_artists(artist)
-    @artists << artist
+    @artists << Artist.new(artist)
   end
 
   def find_photograph_by_id(id)
-    @photographs.find {|photograph| photograph[:id] == id }
+    @photographs.find {|photograph| photograph.id == id }
   end
 
   def find_artist_by_id(id)
-    @artists.find {|artist| artist[:id] == id }
+    @artists.find {|artist| artist.id == id }
   end
+
+  # def find_photographs_by_artist(artist)
+  #   @photographs.find {|photograph| photograph[:artist_id] == artist.id}
+  # end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -5,6 +5,55 @@ require './lib/curator'
 class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
+    @artists = [
+      artist_1 = {
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+      },
+      artist_2 = {
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      },
+      artist_3 = {
+        id: "3",
+        name: "Diane Arbus",
+        born: "1923",
+        died: "1971",
+        country: "United States"
+      }
+    ]
+    @photographs = [
+      photo_1 = {
+        id: "1",
+        name: "Rue Mouffetard, Paris (Boy with Bottles)",
+        artist_id: "1",
+        year: "1954"
+      },
+      photo_2 = {
+        id: "2",
+        name: "Moonrise, Hernandez",
+        artist_id: "2",
+        year: "1941"
+      },
+      photo_3 = {
+        id: "3",
+        name: "Identical Twins, Roselle, New Jersey",
+        artist_id: "3",
+        year: "1967"
+      },
+      photo_4 = {
+        id: "4",
+        name: "Child with Toy Hand Grenade in Central Park",
+        artist_id: "3",
+        year: "1962"
+      }
+    ]
   end
 
   def test_it_exists
@@ -20,92 +69,55 @@ class CuratorTest < Minitest::Test
   end
 
   def test_it_can_add_photographs
-    photo_1 = {
-      id: "1",
-      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: "1",
-      year: "1954"
-    }
+    @curator.add_photographs(@photographs[0])
+    @curator.add_photographs(@photographs[1])
 
-    photo_2 = {
-      id: "2",
-      name: "Moonrise, Hernandez",
-      artist_id: "2",
-      year: "1941"
-    }
-
-    @curator.add_photographs(photo_1)
-    @curator.add_photographs(photo_2)
-
-    assert_equal [photo_1, photo_2], @curator.photographs
-    assert_equal photo_1, @curator.photographs.first
+    assert_equal "1", @curator.photographs.first.id
+    assert_equal "2", @curator.photographs.last.id
+    assert_equal "Moonrise, Hernandez", @curator.photographs.last.name
   end
 
   def test_it_can_add_artists
-    artist_1 = {
-      id: "1",
-      name: "Henri Cartier-Bresson",
-      born: "1908",
-      died: "2004",
-      country: "France"
-    }
+    @curator.add_artists(@artists[0])
+    @curator.add_artists(@artists[1])
 
-    artist_2 = {
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    }
-
-    @curator.add_artists(artist_1)
-    @curator.add_artists(artist_2)
-
-    assert_equal [artist_1, artist_2], @curator.artists
-    assert_equal artist_1, @curator.artists.first
+    assert_equal "1", @curator.artists.first.id
+    assert_equal "2", @curator.artists.last.id
+    assert_equal "Henri Cartier-Bresson", @curator.artists.first.name
   end
 
   def test_it_can_find_a_photograph_by_id
-    photo_1 = {
-      id: "1",
-      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: "1",
-      year: "1954"
-    }
+    @curator.add_photographs(@photographs[0])
+    @curator.add_photographs(@photographs[1])
 
-    photo_2 = {
-      id: "2",
-      name: "Moonrise, Hernandez",
-      artist_id: "2",
-      year: "1941"
-    }
+    actual = @curator.find_photograph_by_id("2")
 
-    @curator.add_photographs(photo_1)
-    @curator.add_photographs(photo_2)
-
-    assert_equal photo_2, @curator.find_photograph_by_id("2")
+    assert_equal "2", actual.id
+    assert_equal "Moonrise, Hernandez", actual.name
   end
 
   def test_it_can_find_a_artist_by_id
-    artist_1 = {
-      id: "1",
-      name: "Henri Cartier-Bresson",
-      born: "1908",
-      died: "2004",
-      country: "France"
-    }
 
-    artist_2 = {
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    }
+    @curator.add_artists(@artists[0])
+    @curator.add_artists(@artists[1])
 
-    @curator.add_artists(artist_1)
-    @curator.add_artists(artist_2)
+    actual = @curator.find_artist_by_id("2")
 
-    assert_equal artist_2, @curator.find_artist_by_id("2")
+    assert_equal "2", actual.id
+    assert_equal "Ansel Adams", actual.name
   end
+
+  # def test_it_can_find_photographs_by_artist
+  #   @curator.add_artists(@artists[0])
+  #   @curator.add_artists(@artists[1])
+  #   @curator.add_artists(@artists[2])
+  #   @curator.add_photographs(@photographs[0])
+  #   @curator.add_photographs(@photographs[1])
+  #   @curator.add_photographs(@photographs[2])
+  #   @curator.add_photographs(@photographs[3])
+  #
+  #   diane_arbus = @curator.find_artist_by_id("3")
+  #
+  #   assert_equal @curator.photographs[(2..3)], @curator.find_photographs_by_artist(diane_arbus)
+  # end
 end
