6b85cf14c16f21fce35bd37ebd6ece9f9838ef03
Tony Robertson <tonyr729@gmail.com>
2018-10-31 09:22:09 -0600

    Add tests and implementation for artist attributes

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -17,4 +17,24 @@ class ArtistTest < Minitest::Test
   def test_it_exists
     assert_instance_of Artist, @artist
   end
+
+  def test_it_has_an_id
+    assert_equal @attributes[:id], @artist.id
+  end
+
+  def test_it_has_an_name
+    assert_equal @attributes[:name], @artist.name
+  end
+
+  def test_it_has_an_born
+    assert_equal @attributes[:born], @artist.born
+  end
+
+  def test_it_has_an_died
+    assert_equal @attributes[:died], @artist.died
+  end
+
+  def test_it_has_an_country
+    assert_equal @attributes[:country], @artist.country
+  end
 end
